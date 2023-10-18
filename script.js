e9b4c5853f0d0fdb1fe09a8ed88c1819d80ab225
horodeski <geovana.horodeski06@gmail.com>
2023-10-17 21:39:37 -0300

    Mudando os rumos do trabalho

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const nav = document.querySelector('nav');
+const logo = document.querySelector('.logo');
 
 window.addEventListener('scroll', () => {
   if (window.scrollY > 5) {
     nav.style.boxShadow = '0px 0px 10px rgba(0, 0, 0, 0.2)';
     nav.style.background = 'white';
+    nav.style.height = '5em';
+    logo.style.width = '5em';
   } else {
+    logo.style.width = '11em';
+    nav.style.height = '11em';
     nav.style.boxShadow = 'none';
     nav.style.background = 'transparent';
   }
 });
+
