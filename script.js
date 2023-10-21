5f6cbbc73c6ce4ff06ab1612cc92593610323cda
horodeski <geovana.horodeski06@gmail.com>
2023-10-21 14:51:17 -0300

    Atualização

--- script.js
+++ script.js
@@ -15,3 +15,34 @@ window.addEventListener('scroll', () => {
   }
 });
 
+const faqItems = document.querySelectorAll('.faq-item');
+
+faqItems.forEach(item => {
+  const question = item.querySelector('.faq-question');
+  const answer = item.nextElementSibling;
+  const icon = item.querySelector('i');
+
+  item.addEventListener('click', () => {
+    faqItems.forEach(otherItem => {
+      if (otherItem !== item) {
+        const otherAnswer = otherItem.nextElementSibling;
+        const otherIcon = otherItem.querySelector('i');
+
+        otherAnswer.classList.remove('active');
+        otherIcon.classList.remove('active');
+        otherAnswer.style.maxHeight = "0";
+        otherAnswer.style.padding = "0";
+      }
+    });
+
+    answer.classList.toggle('active');
+    icon.classList.toggle('active');
+    if (answer.classList.contains('active')) {
+      answer.style.maxHeight = "8em";
+      answer.style.padding = "2em 1em";
+    } else {
+      answer.style.padding = "0";
+      answer.style.maxHeight = "0";
+    }
+  });
+});
