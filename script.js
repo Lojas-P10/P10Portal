9e4008b5c6e8c816e591e55b9752fe2047840d9a
horodeski <geovana.horodeski06@gmail.com>
2023-10-22 14:09:18 -0300

    [Resolves #10] Transformando em responsivo

--- script.js
+++ script.js
@@ -1,43 +1,59 @@
-const nav = document.querySelector('nav');
-const logo = document.querySelector('.logo');
+document.addEventListener("DOMContentLoaded", () => {
+  const navLinks = document.querySelectorAll(".nav-links");
+  const nav = document.querySelector("nav");
 
-window.addEventListener('scroll', () => {
-  if (window.scrollY > 5) {
-    nav.style.boxShadow = '0px 0px 10px rgba(0, 0, 0, 0.2)';
-    nav.style.background = 'white';
-    nav.style.height = '5em';
-    logo.style.width = '5em';
-  } else {
-    logo.style.width = '11em';
-    nav.style.height = '11em';
-    nav.style.boxShadow = 'none';
-    nav.style.background = 'transparent';
+  const minWidthForEffect = 1100; // Largura mínima da tela para o efeito
+
+  function handleScroll() {
+    if (window.innerWidth >= minWidthForEffect) {
+      if (window.scrollY > 5) {
+        nav.style.boxShadow = "0px 0px 10px rgba(0, 0, 0, 0.2)";
+        nav.style.background = "#fcfcfc";
+        nav.style.height = "6em";
+        navLinks.forEach((link) => {
+          link.style.color = "#2c2c2b";
+        });
+      } else {
+        nav.style.height = "11em";
+        nav.style.boxShadow = "none";
+        navLinks.forEach((link) => {
+          link.style.color = "#fcfcfc";
+        });
+        nav.style.background = "transparent";
+      }
+    } else {
+      nav.style.background = "white";
+      nav.style.height = "6em";
+    }
   }
+
+  window.addEventListener("scroll", handleScroll);
+
+  handleScroll();
 });
 
-const faqItems = document.querySelectorAll('.faq-item');
+const faqItems = document.querySelectorAll(".faq-item");
 
-faqItems.forEach(item => {
-  const question = item.querySelector('.faq-question');
+faqItems.forEach((item) => {
+  const question = item.querySelector(".faq-question");
   const answer = item.nextElementSibling;
-  const icon = item.querySelector('i');
+  const icon = item.querySelector("i");
 
-  item.addEventListener('click', () => {
-    faqItems.forEach(otherItem => {
+  item.addEventListener("click", () => {
+    faqItems.forEach((otherItem) => {
       if (otherItem !== item) {
         const otherAnswer = otherItem.nextElementSibling;
-        const otherIcon = otherItem.querySelector('i');
-
-        otherAnswer.classList.remove('active');
-        otherIcon.classList.remove('active');
+        const otherIcon = otherItem.querySelector("i");
+        otherAnswer.classList.remove("active");
+        otherIcon.classList.remove("active");
         otherAnswer.style.maxHeight = "0";
         otherAnswer.style.padding = "0";
       }
     });
 
-    answer.classList.toggle('active');
-    icon.classList.toggle('active');
-    if (answer.classList.contains('active')) {
+    answer.classList.toggle("active");
+    icon.classList.toggle("active");
+    if (answer.classList.contains("active")) {
       answer.style.maxHeight = "8em";
       answer.style.padding = "2em 1em";
     } else {
@@ -46,3 +62,32 @@ faqItems.forEach(item => {
     }
   });
 });
+
+function openModal() {
+  var modal = document.getElementById("myModal");
+  modal.style.display = "flex";
+
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "Escape") {
+      closeModal();
+    }
+  });
+
+  modal.addEventListener("click", function (event) {
+    if (event.target === modal) {
+      closeModal();
+    }
+  });
+}
+
+function closeModal() {
+  var modal = document.getElementById("myModal");
+  modal.style.display = "none";
+
+  document.removeEventListener("keydown", closeModal);
+
+  modal.removeEventListener("click", closeModal);
+}
+
+document.getElementById("openModal").addEventListener("click", openModal);
+document.getElementById("closeModal").addEventListener("click", closeModal);
